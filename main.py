53f2ef70ca62a585d25eaafd32a8c71060379aed
Martin Rakús <martin.rakus1@gmail.com>
2021-09-29 17:41:53 +0200

    Version 1.0 of menu

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QLabel, QPushButton, QGridLayout, QSizePolicy, QMainWindow, QMenuBar, \
+    QSpinBox, QTableWidget, QTableWidgetItem, QLineEdit, QComboBox, QHeaderView, QMessageBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont, QIcon
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def quit():
+    exit()
 
+def main():
+    app = QApplication(sys.argv)
+    windows = QWidget()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    screen = QDesktopWidget().screenGeometry()
 
+    windows.resize(screen.width(), screen.height())
+    windows.setWindowTitle('Kotahi')
+    windows.setWindowIcon(QIcon('icon.png'))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    menu_layout = QGridLayout(windows)
+    menu_layout.setContentsMargins(screen.width() % 3, screen.height() % 10, screen.width() % 3, screen.height() % 10)
+#BACKGROUND
+    windows.setStyleSheet("border-image: url(background.jpg); background-attachment: fixed")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    windows.showFullScreen()
+
+    title = QPushButton(windows)
+    title.setText("Kotahi")
+#    title.setFixedSize(screen.width() // 5, screen.height() // 10)
+    menu_layout.addWidget(title, 0, 2)
+
+    profile1 = QPushButton(windows)
+    profile1.setText("Create new profile")
+    profile1.setStyleSheet('QPushButton {border-color: #A3C1DA; color: red;}')
+#    profile1.clicked.connect()
+    menu_layout.addWidget(profile1, 1, 2)
+
+    profile2 = QPushButton(windows)
+    profile2.setText("Create new profile")
+    #    profile1.clicked.connect()
+    menu_layout.addWidget(profile2, 2, 2)
+
+    profile3 = QPushButton(windows)
+    profile3.setText("Create new profile")
+    #    profile1.clicked.connect()
+    menu_layout.addWidget(profile3, 3, 2)
+
+    profile4 = QPushButton(windows)
+    profile4.setText("Create new profile")
+    #    profile1.clicked.connect()
+    menu_layout.addWidget(profile4, 4, 2)
+
+    profile5 = QPushButton(windows)
+    profile5.setText("Create new profile")
+    #    profile1.clicked.connect()
+    menu_layout.addWidget(profile5, 5, 2)
+
+    quitButton = QPushButton(windows)
+    quitButton.setText("Quit game")
+    quitButton.clicked.connect(quit)
+    #    quitButton.setFixedSize(screen.width() // 5, screen.height() // 10)
+    menu_layout.addWidget(quitButton, 6, 2)
+
+    shop = QPushButton(windows)
+    shop.setText("Quit game")
+    shop.clicked.connect(quit)
+    #    quitButton.setFixedSize(screen.width() // 5, screen.height() // 10)
+    menu_layout.addWidget(shop, 10, 0)
+
+    languageButton = QPushButton(windows)
+    languageButton.setText("Quit game")
+    languageButton.clicked.connect(quit)
+    #    quitButton.setFixedSize(screen.width() // 5, screen.height() // 10)
+    menu_layout.addWidget(languageButton, 10, 4)
+
+    sys.exit(app.exec_())
+
+main()
