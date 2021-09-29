bfd54378b0fa3157fcacc33a3c6b2310fdfed7cd
Eltody <tomas.zatko.ms@gmail.com>
2021-09-29 20:52:18 +0200

    Prevod zakladnej logiky BE do funkcii

--- backend.py
+++ backend.py
@@ -21,34 +21,41 @@ def Main():
 
     # rozdanie 7 kariet hracovi a botovi, pripadne druhemu botovi a odstranenie kariet z balicku
     if numberOfPlayers == 2 or numberOfPlayers == 3:
-        print(len(cards_in_stack))
+        print('cards_in_stack:', len(cards_in_stack))
         for i in range(7):
             cardToRemoveFromStack = random.choice(cards_in_stack)
             playerCards.append(cardToRemoveFromStack)
             cards_in_stack.remove(cardToRemoveFromStack)
-        print(playerCards)
-        print(len(cards_in_stack))
+        print('playerCards:', playerCards)
+        print('cards_in_stack:', len(cards_in_stack))
 
         for i in range(7):
             cardToRemoveFromStack = random.choice(cards_in_stack)
             botCards.append(cardToRemoveFromStack)
             cards_in_stack.remove(cardToRemoveFromStack)
-        print(botCards)
-        print(len(cards_in_stack))
+        print('botCards:', botCards)
+        print('cards_in_stack:', len(cards_in_stack))
         if numberOfPlayers == 3:
             for i in range(7):
                 cardToRemoveFromStack = random.choice(cards_in_stack)
                 second_botCards.append(cardToRemoveFromStack)
                 cards_in_stack.remove(cardToRemoveFromStack)
-            print(second_botCards)
-            print(len(cards_in_stack))
+            print('second_botCards:', second_botCards)
+            print('cards_in_stack:', len(cards_in_stack))
+
     # Otocenie vrchnej karty z balicku licem nahoru a polozenie na stol ako zaklad odhadzovacej hromadky, na kt. budu hraci odhadzovat svoje karty
     thrownAwayCards = cards_in_stack[-1]
     cardToRemoveFromStack = cards_in_stack[-1]
     cards_in_stack.remove(cardToRemoveFromStack)
-    print(thrownAwayCards)
-    print(len(cards_in_stack))
+    print('Vrchna karta na odhadzovacej hromadke:', thrownAwayCards)
+    print('cards_in_stack:', len(cards_in_stack))
 
+    # volanie funkcii na tah hraca a bota
+    playerMove(thrownAwayCards, playerCards)
+    botMove(thrownAwayCards, botCards)
+    exit(0) # uspesne ukoncenie programu
+
+def playerMove(thrownAwayCards, playerCards):
     # na tahu je hrac, na odhadzovaciu hromadku moze odhodit len kartu z ruky, ktora ma rovnaku farbu alebo ciselnu hodnotu ako vrchna karta na odhadzovacej hromadke
     # TODO urobit to aj pre rovnaky symbol
     if playerCards != 0:
@@ -70,7 +77,6 @@ def Main():
 
         # vrati vsetky hracove karty, ktore maju rovnaku farbu ako farba karty na hromadke
         sameColorPlayerCardsToThrowAway = [i for i in playerCards if colorOfThrownAwayCard in i]
-        print(sameColorPlayerCardsToThrowAway)
 
         # zistenie cisla karty na odhadzovacej hromadke
         if 'Zero' in thrownAwayCards:
@@ -100,11 +106,10 @@ def Main():
 
         # vrati vsetky hracove karty, ktore maju rovnake cislo ako cislo karty na hromadke
         sameNumberPlayerCardsToThrowAway = [i for i in playerCards if numberOfThrownAwayCard in i]
-        print(sameNumberPlayerCardsToThrowAway)
 
         # spojenie farbenych a ciselnych kariet, ktore moze hrac odhodit na hromadku
         allowedPlayerCardsToThrowAway = sameColorPlayerCardsToThrowAway + sameNumberPlayerCardsToThrowAway
-        print(allowedPlayerCardsToThrowAway)
+        print('allowedPlayerCardsToThrowAway:', allowedPlayerCardsToThrowAway)
 
         # TODO v tomto bode mam v BE zistene, ktore karty moze hrac odhodit na hromadku. Ale ako zistim, ktoru kartu sa hrac rozhodne odhodit? To musi prist asi z FE ako premenna playerCardToThrowAway.
 
@@ -119,9 +124,57 @@ def Main():
         #     cards_in_stack.remove(cardToRemoveFromStack)            # odstranenie vybranej karty z balicku
         #     # TODO pokial tato vybrana karta z balicku dovoluje hracovi odhodit tuto novu kartu, moze tak urobit, inak sa tah presunie na tah bota
 
+def botMove(thrownAwayCards, botCards):
+    if botCards != 0:
+        allowedBotCardsToThrowAway = []  # karty, ktore moze bot odhodit na odhadzovaciu hromadku
 
+        # zistenie farby karty na odhadzovacej hromadke
+        if 'red' in thrownAwayCards:
+            colorOfThrownAwayCard = 'red'
+        elif 'yellow' in thrownAwayCards:
+            colorOfThrownAwayCard = 'yellow'
+        elif 'green' in thrownAwayCards:
+            colorOfThrownAwayCard = 'green'
+        elif 'blue' in thrownAwayCards:
+            colorOfThrownAwayCard = 'blue'
+        else:
+            print('[color] ThrownAwayCard is a wild card, code is not ready for this yet.')  # TODO implement wild cards, reverse cards, draw cards, skip cards
+            colorOfThrownAwayCard = 'wild card'
 
+        # vrati vsetky botove karty, ktore maju rovnaku farbu ako farba karty na hromadke
+        sameColorBotCardsToThrowAway = [i for i in botCards if colorOfThrownAwayCard in i]
 
-    exit(0) # uspesne ukoncenie programu
+        # zistenie cisla karty na odhadzovacej hromadke
+        if 'Zero' in thrownAwayCards:
+            numberOfThrownAwayCard = 'Zero'
+        elif 'One' in thrownAwayCards:
+            numberOfThrownAwayCard = 'One'
+        elif 'Two' in thrownAwayCards:
+            numberOfThrownAwayCard = 'Two'
+        elif 'Three' in thrownAwayCards:
+            numberOfThrownAwayCard = 'Three'
+        elif 'Four' in thrownAwayCards:
+            numberOfThrownAwayCard = 'Four'
+        elif 'Five' in thrownAwayCards:
+            numberOfThrownAwayCard = 'Five'
+        elif 'Six' in thrownAwayCards:
+            numberOfThrownAwayCard = 'Six'
+        elif 'Seven' in thrownAwayCards:
+            numberOfThrownAwayCard = 'Seven'
+        elif 'Eight' in thrownAwayCards:
+            numberOfThrownAwayCard = 'Eight'
+        elif 'Nine' in thrownAwayCards:
+            numberOfThrownAwayCard = 'Nine'
+        else:
+            print('[number] ThrownAwayCard is a wild card, code is not ready for this yet.')  # TODO implement wild cards, reverse cards, draw cards, skip cards
+            numberOfThrownAwayCard = 'wild card'
+
+        # vrati vsetky hracove karty, ktore maju rovnake cislo ako cislo karty na hromadke
+        sameNumberBotCardsToThrowAway = [i for i in botCards if numberOfThrownAwayCard in i]
+
+        # spojenie farbenych a ciselnych kariet, ktore moze hrac odhodit na hromadku
+        allowedBotCardsToThrowAway = sameColorBotCardsToThrowAway + sameNumberBotCardsToThrowAway
+        print('allowedBotCardsToThrowAway:', allowedBotCardsToThrowAway)
+
 # Zavolanie fce Main
 Main()
